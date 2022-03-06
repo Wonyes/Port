37e327c2d65c70319c119077699e12db8bb77f6e
mangoo <cjo9701@gmail.com>
2022-03-06 20:16:45 +0900

    0306

--- js/main.js
+++ js/main.js
@@ -126,4 +126,13 @@ $(document).ready(function() {
     menuToggle.onclick = function(){
         navigation.classList.toggle('open')
     }
+    const list = document.querySelectorAll('.list');
+    function activeLink(){
+        list.forEach((item) =>
+        item.classList.remove('active'));
+        this.classList.add('active');
+    }
+    list.forEach((item) =>
+    item.addEventListener('click',activeLink));
+    
 })
